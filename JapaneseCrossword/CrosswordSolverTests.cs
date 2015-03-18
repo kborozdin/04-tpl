302bd90e1301f207f9e2e4559ef568b1d1434ce2
kborozdin <kirill.borozdin@gmail.com>
2015-03-18 19:09:17 +0500

    Changed unit tests

--- JapaneseCrossword/CrosswordSolverTests.cs
+++ JapaneseCrossword/CrosswordSolverTests.cs
@@ -6,93 +6,122 @@ namespace JapaneseCrossword
     [TestFixture]
     public class CrosswordSolverTests
     {
-        private CrosswordSolver solver;
+        private CrosswordSolver[] solvers;
 
         [TestFixtureSetUp]
         public void SetUp()
         {
 			var lineCorrectnessChecker = new LineCorrectnessChecker();
 			var lineUpdater = new LineUpdater(lineCorrectnessChecker);
-			var dimensionUpdater = new MultiThreadDimensionUpdater(lineUpdater);
-			solver = new CrosswordSolver(dimensionUpdater);
+
+			var oneDimensionUpdater = new OneThreadDimensionUpdater(lineUpdater);
+			var multiDimensionUpdater = new MultiThreadDimensionUpdater(lineUpdater);
+
+			solvers = new CrosswordSolver[2];
+			solvers[0] = new CrosswordSolver(oneDimensionUpdater);
+			solvers[1] = new CrosswordSolver(multiDimensionUpdater);
         }
 
         [Test]
         public void InputFileNotFound()
         {
-            var solutionStatus = solver.Solve(Path.GetRandomFileName(), Path.GetRandomFileName());
-            Assert.AreEqual(SolutionStatus.BadInputFilePath, solutionStatus);
+			foreach (var solver in solvers)
+			{
+				var solutionStatus = solver.Solve(Path.GetRandomFileName(), Path.GetRandomFileName());
+				Assert.AreEqual(SolutionStatus.BadInputFilePath, solutionStatus);
+			}
         }
 
         [Test]
         public void IncorrectOutputFile()
         {
-            var inputFilePath = @"TestFiles\SampleInput.txt";
-            var outputFilePath = "///.&*#";
-            var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
-            Assert.AreEqual(SolutionStatus.BadOutputFilePath, solutionStatus);
+			foreach (var solver in solvers)
+			{
+				var inputFilePath = @"TestFiles\SampleInput.txt";
+				var outputFilePath = "///.&*#";
+				var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
+				Assert.AreEqual(SolutionStatus.BadOutputFilePath, solutionStatus);
+			}
         }
 
         [Test]
         public void IncorrectCrossword()
         {
-            var inputFilePath = @"TestFiles\IncorrectCrossword.txt";
-            var outputFilePath = Path.GetRandomFileName();
-            var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
-            Assert.AreEqual(SolutionStatus.IncorrectCrossword, solutionStatus);
+			foreach (var solver in solvers)
+			{
+				var inputFilePath = @"TestFiles\IncorrectCrossword.txt";
+				var outputFilePath = Path.GetRandomFileName();
+				var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
+				Assert.AreEqual(SolutionStatus.IncorrectCrossword, solutionStatus);
+			}
         }
 
         [Test]
         public void Simplest()
         {
-            var inputFilePath = @"TestFiles\SampleInput.txt";
-            var outputFilePath = Path.GetRandomFileName();
-            var correctOutputFilePath = @"TestFiles\SampleInput.solved.txt";
-            var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
-            Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
-            CollectionAssert.AreEqual(File.ReadAllText(correctOutputFilePath), File.ReadAllText(outputFilePath));
+			foreach (var solver in solvers)
+			{
+				var inputFilePath = @"TestFiles\SampleInput.txt";
+				var outputFilePath = Path.GetRandomFileName();
+				var correctOutputFilePath = @"TestFiles\SampleInput.solved.txt";
+				var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
+				Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
+				CollectionAssert.AreEqual(File.ReadAllText(correctOutputFilePath), File.ReadAllText(outputFilePath));
+			}
         }
 
         [Test]
         public void Car()
         {
-            var inputFilePath = @"TestFiles\Car.txt";
-            var outputFilePath = Path.GetRandomFileName();
-            var correctOutputFilePath = @"TestFiles\Car.solved.txt";
-            var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
-            Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
-            CollectionAssert.AreEqual(File.ReadAllText(correctOutputFilePath), File.ReadAllText(outputFilePath));
+			foreach (var solver in solvers)
+			{
+				var inputFilePath = @"TestFiles\Car.txt";
+				var outputFilePath = Path.GetRandomFileName();
+				var correctOutputFilePath = @"TestFiles\Car.solved.txt";
+				var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
+				Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
+				CollectionAssert.AreEqual(File.ReadAllText(correctOutputFilePath), File.ReadAllText(outputFilePath));
+			}
         }
 
         [Test]
         public void Flower()
         {
-            var inputFilePath = @"TestFiles\Flower.txt";
-            var outputFilePath = Path.GetRandomFileName();
-            var correctOutputFilePath = @"TestFiles\Flower.solved.txt";
-            var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
-            Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
-            CollectionAssert.AreEqual(File.ReadAllText(correctOutputFilePath), File.ReadAllText(outputFilePath));
+			foreach (var solver in solvers)
+			{
+				var inputFilePath = @"TestFiles\Flower.txt";
+				var outputFilePath = Path.GetRandomFileName();
+				var correctOutputFilePath = @"TestFiles\Flower.solved.txt";
+				var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
+				Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
+				CollectionAssert.AreEqual(File.ReadAllText(correctOutputFilePath), File.ReadAllText(outputFilePath));
+			}
         }
 
         [Test]
         public void Winter()
         {
-            var inputFilePath = @"TestFiles\Winter.txt";
-            var outputFilePath = Path.GetRandomFileName();
-            //var correctOutputFilePath = @"TestFiles\Winter.solved.txt";
-            var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
-            Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
-            //CollectionAssert.AreEqual(File.ReadAllText(correctOutputFilePath), File.ReadAllText(outputFilePath));
+			foreach (var solver in solvers)
+			{
+				var inputFilePath = @"TestFiles\Winter.txt";
+				var outputFilePath = Path.GetRandomFileName();
+				//var correctOutputFilePath = @"TestFiles\Winter.solved.txt";
+				var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
+				Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
+				//CollectionAssert.AreEqual(File.ReadAllText(correctOutputFilePath), File.ReadAllText(outputFilePath));
+			}
         }
 
 		[Test]
 		public void SuperBig()
 		{
-			var inputFilePath = @"TestFiles\SuperBig.txt";
-			var outputFilePath = Path.GetRandomFileName();
-			var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
-			Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
+			foreach (var solver in solvers)
+			{
+				var inputFilePath = @"TestFiles\SuperBig.txt";
+				var outputFilePath = Path.GetRandomFileName();
+				var solutionStatus = solver.Solve(inputFilePath, outputFilePath);
+				Assert.AreEqual(SolutionStatus.Solved, solutionStatus);
+			}
 		}
     }
 }
